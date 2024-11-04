dfb71fd616a280c8ff7349994c069a32ff054e1b
Oleksiy Mykolayovych <uaspace7@gmail.com>
2024-11-04 09:31:58 +0200

    Створено проєкт для домашніх завдань. Додано перше завдання.

---
+++ Task-1/scripts/main.js
@@ -0,0 +1,70 @@
+/* ----- task-1 ----- */
+let varA; let varB; let varC; let varD; let varE; let varF;
+let varG; let varH; let varI; let varJ; let varK; let varL; let varM;
+
+varA = 'hello';
+console.log(varA);
+
+varB = 'owu';
+console.log(varB);
+
+varC = 'com';
+console.log(varC);
+
+varD = 'ua';
+console.log(varD);
+
+varE = 1;
+console.log(varE);
+
+varF = 10;
+console.log(varF);
+
+varG = -999;
+console.log(varG);
+
+varH = 123;
+console.log(varH);
+
+varI = 3.14;
+console.log(varI);
+
+varJ = 2.7;
+console.log(varJ);
+
+varK = 16;
+console.log(varK);
+
+varL = true;
+console.log(varL);
+
+varM = false;
+console.log(varM);
+
+/* ----- task-2 ----- */
+let firstName; let middleName; let lastName;
+
+firstName = 'Олексій';
+middleName = 'Миколайович';
+lastName = 'Безкоровайний';
+
+let fio = `${firstName} ${middleName} ${lastName}`;
+console.log(fio);
+
+/* ----- task-3 ----- */
+let a = 100; let b = '100'; let c = true;
+
+console.log(typeof a, a);
+console.log(typeof b, b);
+console.log(typeof c, c);
+
+/* ----- task-4 ----- */
+let age;
+
+firstName = prompt("What's your name?");
+middleName = prompt("What's your second name?");
+age = prompt("How old are You?");
+
+fio = `${firstName} ${middleName}`;
+
+console.log(`${fio} - ${age} років`);
