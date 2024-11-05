cce943b16aa716f94884ba5fd7f7cc4b2e26335b
Oleksiy Mykolayovych <uaspace7@gmail.com>
2024-11-05 21:47:27 +0200

    Створено папку Lesson-2. Виконано перші 8 завдань.

---
+++ Lesson-2/scripts/main.js
@@ -0,0 +1,276 @@
+/* ----- 67kfznmiMl ----- */
+let arr = [
+    -20.5,
+    -10
+    - 5,
+    0,
+    '',
+    null,
+    'asd',
+    'qwe',
+    3, 14,
+    456
+];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[8]);
+console.log(arr[9]);
+
+/* ----- LARqoUj5I ----- */
+let arrBookOne = {
+    title: 'Book One',
+    pageCount: '11',
+    genre: 'History'
+}
+let arrBookTwo = {
+    title: 'Book Two',
+    pageCount: '22',
+    genre: 'Melodrama'
+}
+let arrBookThree = {
+    title: 'Book Three',
+    pageCount: '33',
+    genre: 'Comedy'
+}
+
+/* ----- sA3Gg1sCp ----- */
+arrBookOne.authors = [
+    {
+        name: 'Author One One',
+        age: '12'
+    }
+    ,
+    {
+        name: 'Author One Two',
+        age: '23'
+    }
+]
+arrBookTwo.authors = [
+    {
+        name: 'Author Two One',
+        age: '34'
+    },
+    {
+        name: 'Author Two Two',
+        age: '45'
+    }
+]
+arrBookThree.authors = [
+    {
+        name: 'Author Three One',
+        age: '56'
+    },
+    {
+        name: 'Author Three Two',
+        age: '65'
+    }
+]
+
+/* ----- jCHFnEbdmFd ----- */
+let users = [
+    {
+        name: 'Name 1',
+        username: 'User Name 1',
+        password: 'password_1'
+    },
+    {
+        name: 'Name 2',
+        username: 'User Name 2',
+        password: 'password_2'
+    },
+    {
+        name: 'Name 3',
+        username: 'User Name 3',
+        password: 'password_3'
+    },
+    {
+        name: 'Name 4',
+        username: 'User Name 4',
+        password: 'password_4'
+    },
+    {
+        name: 'Name 5',
+        username: 'User Name 5',
+        password: 'password_5'
+    },
+    {
+        name: 'Name 6',
+        username: 'User Name 6',
+        password: 'password_6'
+    },
+    {
+        name: 'Name 7',
+        username: 'User Name 7',
+        password: 'password_7'
+    },
+    {
+        name: 'Name 8',
+        username: 'User Name 8',
+        password: 'password_8'
+    },
+    {
+        name: 'Name 9',
+        username: 'User Name 9',
+        password: 'password_9'
+    },
+    {
+        name: 'Name 10',
+        username: 'User Name 10',
+        password: 'password_10'
+    }
+]
+console.log(users[0].password);
+console.log(users[2].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+/* ----- coYydZuaeEB ----- */
+let arrDayTemperature = {
+    morning: {
+        monday: 0,
+        tuesday: 1,
+        wednesday: 2,
+        thursday: 3,
+        friday: 4,
+        saturday: 5,
+        sunday: 6
+    },
+    day: {
+        monday: 10,
+        tuesday: 11,
+        wednesday: 12,
+        thursday: 13,
+        friday: 14,
+        saturday: 15,
+        sunday: 16
+    },
+    evening: {
+        monday: 5,
+        tuesday: 6,
+        wednesday: 7,
+        thursday: 8,
+        friday: 9,
+        saturday: 10,
+        sunday: 11
+    }
+}
+let arrWeekTemperature = [
+    {
+        morning: 6,
+        day: 16,
+        evening: 11
+    },
+    {
+        morning: 0,
+        day: 10,
+        evening: 5
+    },
+    {
+        morning: 1,
+        day: 11,
+        evening: 6
+    },
+    {
+        morning: 2,
+        day: 12,
+        evening: 7
+    },
+    {
+        morning: 3,
+        day: 13,
+        evening: 8
+    },
+    {
+        morning: 4,
+        day: 14,
+        evening: 9
+    },
+    {
+        morning: 5,
+        day: 15,
+        evening: 10
+    }
+]
+
+/* ----- bAUsaq6LI ----- */
+let x = 5;
+if (x !== 0) {
+    console.log('Вірно');
+}
+else {
+    console.log('Невірно');
+}
+x = 1;
+if (x !== 0) {
+    console.log('Вірно');
+}
+else {
+    console.log('Невірно');
+}
+x = 0;
+if (x !== 0) {
+    console.log('Вірно');
+}
+else {
+    console.log('Невірно');
+}
+x = -3;
+if (x !== 0) {
+    console.log('Вірно');
+}
+else {
+    console.log('Невірно');
+}
+
+/* ----- 3ckURgvs ----- */
+let time = 13;
+if (time >= 0 && time < 15) {
+    console.log('1 quarter of an hour');
+}
+else if (time >= 15 && time < 30) {
+    console.log('2 quarter of an hour');
+}
+else if (time >= 30 && time < 45) {
+    console.log('3 quarter of an hour');
+}
+else if (time >= 45 && time <= 59) {
+    console.log('4 quarter of an hour');
+}
+else {
+    console.log(`minute doesn't exist`);
+}
+
+/* ----- UMoNq4biWGe ----- */
+let day = 13;
+if (day >= 1 && day <= 10) {
+    console.log('first ten days of the month');
+}
+else if (day > 10 && day <= 20) {
+    console.log('second ten days of the month');
+}
+else if (day > 20 && day <= 31) {
+    console.log('third ten days of the month');
+}
+else if (day < 1 || day > 31) {
+    console.log(`day of the month does not exist`);
+}
+
+/* ----- KzrtqyQ ----- */
+
+/* ----- uwsz1RnTQJ1 ----- */
+
+/* ----- iBvqtjEm ----- */
+
+/* ----- awLXL6TBzg ----- */
